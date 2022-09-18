09ef5cf54a5d5ae7951b51c2832a6b2ecee9237b
Oron Arish <oarish96@gmail.com>
2022-09-18 13:44:13 +0300

    some changes

--- src/components/CopyRights.jsx
+++ src/components/CopyRights.jsx
@@ -5,8 +5,8 @@ function CopyRights() {
 return (
     <div class="bottom-container">
     <a class="footer-link" href="https://www.linkedin.com/in/oron-arish-0809a723a/">LinkedIn</a>
-    <a class="footer-link" href="https://twitter.com/">My CV</a>
-    <a class="footer-link" href="https://www.appbrewery.co/">Website</a>
+    <a class="footer-link" href="https://docdro.id/ZeXs4Y4">My CV</a>
+    <a class="footer-link" href="https://github.com/OronArish/FirstProjectNew.git">GitHub</a>
     <p class = "copyright">© Oron Arish.</p>
   </div>
 )
